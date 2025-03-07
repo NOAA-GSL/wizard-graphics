7a514f057156794d3b0287aa97e773ece3f38e5b
Rob Howlett <rob.howlett@noaa.gov>
2025-03-07 12:25:12 -0700

    removed some unnecessary files

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        viteStaticCopy({
-            targets: [
-                {
-                    src: 'src/generated.json',
-                    dest: './static/.',
-                },
-            ],
-        }),
-    ],
+    plugins: [react()],
     resolve: {
         alias: {
             'desi-graphics': path.resolve('./src'),
