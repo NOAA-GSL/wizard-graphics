18ad99ed28253afcd5ca82f9dc4d2d0122c86b13
Rob Howlett <rob.howlett@noaa.gov>
2025-09-17 11:21:12 -0700

    fixed climo colorbar

--- library/src/conf/configFieldsDefault.js
+++ library/src/conf/configFieldsDefault.js
@@ -71,6 +71,59 @@ const defaults = {
             isRightCap: true,
             ticks: 'byColorLevels',
         },
+        // Climatology colorbar
+        climo: {
+            colors: [
+                'rgb(3, 10, 60)', // 0.0000–0.0032   (extreme low tail)
+                'rgb(13, 30, 80)', // 0.0032–0.023    (-4.0σ to -3.5σ)
+                'rgb(23, 50, 99)', // 0.023–0.135     (-3.5σ to -3.0σ)
+                'rgb(33, 70, 119)', // 0.135–0.621     (-3.0σ to -2.5σ)
+                'rgb(43, 91, 138)', // 0.621–2.275     (-2.5σ to -2.0σ)
+                'rgb(53, 111, 158)', // 2.275–6.681     (-2.0σ to -1.5σ)
+                'rgb(63, 131, 177)', // 6.681–15.866    (-1.5σ to -1.0σ)
+                'rgb(73,151,197)', // 15.866–30.854   (-1.0σ to -0.5σ)
+                'rgba(0,0,0,0)', // 30.854–50.000   (transparent)
+                'rgba(0,0,0,0)', // 50.000–69.146   (transparent)
+                'rgb(241,158,124)', // 69.146–84.134   (+0.5σ to +1.0σ)
+                'rgb(228,128,102)', // 84.134–93.319   (+1.0σ to +1.5σ)
+                'rgb(216,101,81)', // 93.319–97.725   (+1.5σ to +2.0σ)
+                'rgb(201,70,65)', // 97.725–99.379   (+2.0σ to +2.5σ)
+                'rgb(185,39,50)', // 99.379–99.865   (+2.5σ to +3.0σ)
+                'rgb(164,19,40)', // 99.865–99.977   (+3.0σ to +3.5σ)
+                'rgb(131,9,34)', // 99.977–99.9968  (+3.5σ to +4.0σ)
+                'rgb(100,5,25)', // 99.9968–100.000 (extreme high tail)
+            ],
+            colorLevels: [
+                0.01, 0.5, 1, 3, 5, 10, 15, 30, 50.0, 70, 85, 90, 95, 97, 99, 99.5, 99.99,
+            ],
+            colorType: 'scaleThreshold',
+            contourLevels: [
+                0.01, 0.5, 1, 3, 5, 10, 15, 30, 50.0, 70, 85, 90, 95, 97, 99, 99.5, 99.99,
+            ],
+            isLeftCap: true,
+            isRightCap: true,
+            ticks: 'byColorLevels',
+        },
+        // When making a consolidated config, we probably need to add these fields for ptype refc colorbar
+        prain: {
+            colors: [
+                'rgba(150,150,150,0)',
+                'rgba(127,255,0,1)',
+                'rgb(127,255,0)',
+                'rgb(85,195,0)',
+                'rgb(42,136,0)',
+                'rgb(0,76,0)',
+                'rgb(0,76,0)',
+            ],
+            colorLevels: [1, 5, 15, 25, 35, 50],
+            colorType: 'scaleThreshold',
+            contourLevels: [1, 5, 15, 25, 35, 50],
+            isLeftCap: false,
+            isRightCap: false,
+            ticks: 'byColorLevels',
+            // unfortunately we can't put the legend name in here, ie `nameLegend: "Rain Reflectivity"`
+        },
+
         // Timing colorbar
         timing: {
             colors: ['rgba(0,0,0,0)', 'rgb(16, 89, 140)', 'rgb(250, 247, 179)'],
